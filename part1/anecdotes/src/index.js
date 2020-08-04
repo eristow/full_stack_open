d84376682c290acf70ebd4a5d3f2c97862c5a133
Evan Ristow <evan.ristow1@gmail.com>
2020-08-04 10:10:39 -0400

    Anecdotes project in part 1

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Anecdote = ({ text, anecdote, points }) => (
+  <>
+    <h1>{text}</h1>
+    <p>{anecdote}</p>
+    <p>{`has ${points} votes`}</p>
+  </>
+);
+
+const AnecdoteOfTheDay = ({ anecdotes, points, setPoints }) => {
+  const [selected, setSelected] = useState(0);
+
+  const onClickVote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  };
+  const onClickNewAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  return (
+    <div>
+      <Anecdote
+        text="Anecdote of the day"
+        anecdote={anecdotes[selected]}
+        points={points[selected]}
+      />
+      <Button handleClick={onClickVote} text="Vote"></Button>
+      <Button handleClick={onClickNewAnecdote} text="New Anecdote"></Button>
+    </div>
+  );
+};
+
+const MostVotesAnecdote = ({ anecdotes, points }) => {
+  const mostVotes = points.indexOf(Math.max(...points));
+  return (
+    <div>
+      <Anecdote
+        text="Anecdote with most votes"
+        anecdote={anecdotes[mostVotes]}
+        points={points[mostVotes]}
+      />
+    </div>
+  );
+};
+
+const App = ({ anecdotes }) => {
+  const [points, setPoints] = useState(
+    Array.apply(null, new Array(anecdotes.length)).map(
+      Number.prototype.valueOf,
+      0,
+    ),
+  );
+
+  return (
+    <div>
+      <AnecdoteOfTheDay
+        anecdotes={anecdotes}
+        points={points}
+        setPoints={setPoints}
+      />
+      <MostVotesAnecdote anecdotes={anecdotes} points={points} />
+    </div>
+  );
+};
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
