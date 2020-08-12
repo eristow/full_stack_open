b69ee23792c38d768151fa95ed5e1941b703c698
Evan Ristow <evan.ristow1@gmail.com>
2020-08-12 14:59:36 -0400

    Finish lecture and exercises from 2.D

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import personsService from './services/persons';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import Notification from './components/Notification';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [filterText, setFilterText] = useState('');
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [message, setMessage] = useState(null);
+  const [notificationStyle, setNotificationStyle] = useState({
+    color: 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  });
 
   const personsToShow =
     filterText === ''
@@ -18,23 +29,58 @@ const App = () => {
         );
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(response => {
-      setPersons(response.data);
+    personsService.getAll().then(initialPersons => {
+      setPersons(initialPersons);
     });
   }, []);
+
+  const displayMessage = (message, color) => {
+    setNotificationStyle({ ...notificationStyle, color });
+    setMessage(message);
+    setTimeout(() => {
+      setMessage(null);
+    }, 5000);
+  };
 
   const addPerson = event => {
     event.preventDefault();
     const newPerson = { name: newName, number: newNumber };
 
     if (persons.some(person => person.name === newPerson.name)) {
-      alert(`${newName} is already added to phonebook`);
+      const oldPerson = persons.find(person => person.name === newPerson.name);
+      if (
+        window.confirm(
+          `${newName} is already added to the phonebook. Would you like to replace the old number with the new one?`,
+        )
+      ) {
+        personsService
+          .update(oldPerson.id, newPerson)
+          .then(returnedPerson => {
+            setPersons(
+              persons.map(person =>
+                person.id !== returnedPerson.id ? person : returnedPerson,
+              ),
+            );
+            setNewName('');
+            setNewNumber('');
+            displayMessage(`Changed ${returnedPerson.name}'s number.`, 'green');
+          })
+          .catch(error => {
+            displayMessage(
+              `Information of ${newPerson.name} has already been removed from the server.`,
+              'red',
+            );
+          });
+      }
       return;
     }
 
-    setPersons(persons.concat(newPerson));
-    setNewName('');
-    setNewNumber('');
+    personsService.create(newPerson).then(returnedPerson => {
+      setPersons(persons.concat(returnedPerson));
+      setNewName('');
+      setNewNumber('');
+      displayMessage(`Added ${returnedPerson.name}.`, 'green');
+    });
   };
 
   const handleFilterText = event => {
@@ -49,9 +95,23 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleRemovePerson = personDelete => {
+    if (window.confirm(`Delete ${personDelete.name}?`)) {
+      personsService
+        .remove(personDelete.id)
+        .then(removedPerson => {
+          setPersons(persons.filter(person => person.id !== personDelete.id));
+        })
+        .catch(error => {
+          displayMessage(`Error deleting ${personDelete.name}.`);
+        });
+    }
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} style={notificationStyle} />
       <Filter filterText={filterText} handleFilterText={handleFilterText} />
       <h3>Add a new person</h3>
       <PersonForm
@@ -62,7 +122,7 @@ const App = () => {
         handleNewNumber={handleNewNumber}
       />
       <h3>Numbers</h3>
-      <Persons persons={personsToShow} />
+      <Persons persons={personsToShow} removePerson={handleRemovePerson} />
     </div>
   );
 };
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, removePerson }) => {
   return (
     <>
       {persons.map(person => (
-        <p key={person.name}>{`${person.name} ${person.number}`}</p>
+        <p key={person.name}>
+          {`${person.name} ${person.number}`}
+          <button type="button" onClick={() => removePerson(person)}>
+            delete
+          </button>
+        </p>
       ))}
     </>
   );
